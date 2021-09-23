d2849c7470f12d55460e7a0864a43f1aff60aa31
MiPhone <njholeptx@gmail.com>
2021-09-23 16:05:38 -0500

    Another update

--- camcomrade.js
+++ camcomrade.js
@@ -1,73 +1,85 @@
 /*Assets is an object containing component objects, each of which holds both strings referencing the images and alpha channel masks to be used by the botfather Vision API and Match class. These strings are given as relative file paths with the main Cam Comrade installation dir being referenced as the relative origin point. Meanwhile, numerical data sets the maximum percent of pixels which must be detected identically to each asset object in order for a Match to be declared. Finally, the limit number serves to limit the number of Matches sought by the Vision API*/
 
-const assets =
+const assetList = {
     update: {
+        file: "update",
         asset: "assets/ud.png",
         score: 0.95,
         mask: "assets/ud_mask.png",
         limit: 1
     },
     ok: {
+        file: "ok",
         asset: "assets/ok.png",
         score: 0.75,
-        mask: "assets/ok_mask.ping",
+        mask: "assets/ok_mask.png",
         limit: 1
     },
     news: {
+        file: "news",
         asset: "assets/news.png",
         score: 0.95,
-        mask: "assets/news_mask.png"
+        mask: "assets/news_mask.png",
         limit: 1
     },
     start: {
+        file: "start",
         asset: "assets/start.png",
         score: 0.9,
         mask: "assets/start_mask.png",
         limit: 1
     },
     startEx: {
+        file: "start_x",
         asset: "assets/start_x.png",
         score: 0.95,
         mask: "assets/start_x_mask.png",
         limit: 1
     },
     events: {
+        file: "events",
         assets: "assets/events.png",
-        score: 0.95;
+        score: 0.95,
         mask: "assets/events_mask.ping",
         limit: 1
     },
     ex: {
+        file: "x",
         assets: "assets/x.png",
-        score: 0.80;
+        score: 0.80,
         mask: "assets/x_mask.png",
         limit: 1
     },
     chatBox: {
+        file: "chat_frame",
         asset: "assets/chat.png",
         score: 0.85,
         mask: "assets/chat_mask.png",
         limit: 1
     },
     chatExpand: {
+        file: "chat_expand",
         asset: "assets/chat_expand.png",
         score: 0.95,
         mask: "assets/chat_expand_mask.png",
         limit: 1
     },
     charGrey: {
+        file: "char_grey",
         asset: "assets/char_grey.png",
         score: 0.90,
-        mask: "assets/char_grey_mask.png"
+        mask: "assets/char_grey_mask.png",
         limit: 1
     },
     charGlowing: {
+        file: "char_glow",
         asset: "assets/char_glow.png",
         score: 0.85,
         mask: "assets/char_glow_mask.png",
         limit: 1
     },
     red: {
+        file: "red",
         asset: "assets/red.png",
         score: 0.85,
         mask: "assets/red_mask.png",
@@ -75,7 +87,7 @@ const assets =
     }
 }
 
-/*The following four objects hold identifying keys plus the relevant values for all of the fields in the Botfather config window, retrieving them using the Botfather Config.getValue() method*/
+/*The following four objects hold identifying keys plus the relevant values for all of the fields in the Botfather config window, retrieving them using the Botfather Config.getValue() method.*/
 
 const tw = {
     usn: Config.getValue("tw_usn"), 
@@ -91,27 +103,27 @@ const scrn = {
     y: Config.getValue("screen_y"), 
     rectColor: Config.getValue("rect_clr"), 
     rectPix: Config.getValue("rect_px"),
-    take_pics: Config.getValue("take_pics")
+    takePics: Config.getValue("take_pics")
 };
 const adv = {
     dbg: Config.getValue("debug_do"), 
     tick: Config.getValue("tick"), 
     pak: Config.getValue("package"), 
-    take_names: Config.getValue("take_names")
+    takeNames: Config.getValue("take_names")
 };
 
 /*The variable echoesState defines what work phase Cam Camera must currently work within. As an example, consider the "unit" phase: during the initiation phase  bot searches for the start 'button' text, followed by the news pop-up, followed by the update pop-up - ordered by the frequency with which each state is encountered by the bot. This is intended such that should the start button be present - the intended endpoint and desired screenstate for initiation interactions - the bot skips over any of the code relating to the other two possible, less common screenstates. If the potential screenstates were in another order, the update and news IF blocks would be evaluated for truth first despite being detected much less frequently than the start screenstate.*/
 
 var echoes_state = "init";
 
-/*The Interaction class handles the final step of the (detect phase)->(define targets)->(scan for targets)->(act upon flagged targets) bot loop. Provided with a single Botfather library Point object plus an integer between 50 and 250, the instance is constructed accordingly and upon calling the .exec() method a single Tap is fired off to the ADB (Android Debug Bridge) system following a pause equal to the integer value in milliseconds. Should the object however be constructed with a Point, the pause integer, a second Point, and a second integer value between 300 and 700, the .exec() method instead produces a Swipe from the first to the second Point with a duration equal to the second integer in milliseconds, following a pause similar to that which precedes the Tap version of the .exec() method. The purpose of the pause is to 'humanize' the interactions by randomizing the pauses between Taps / Swipes within reasonable bounds, as a lack of minute pauses between touch events would be potentially characteristic of a bot*/
+/*The Interaction class handles the final step of the (detect phase)->(define targets)->(scan for targets)->(act upon flagged targets) bot loop. Provided with a single Botfather library Point object,the instance is constructed such that upon calling the .exec() method a single Tap is fired off to the ADB (Android Debug Bridge) system after a delay of between 100 and 275 milliseconds. Should the object however be constructed with a two Points, the .exec() method instead produces a Swipe from the first to the second Point with a duration between 300 and 500 milliseconds, following a pause similar to that which precedes the Tap version of the .exec() method. The purpose of the pause is to 'humanize' the interactions by randomizing the pauses between Taps / Swipes within reasonable bounds, as a lack of minute pauses between touch events - or a series if pauses with the exact same value each time - would be potentially characteristic of a bot.*/
 class Interaction {
     
-    constructor(tapPoint, pauseIn, swipePoint = null, swipeDur) {
+    constructor(tapPoint, swipePoint = null) {
         this.p1 = tapPoint;
         this.p2 = swipePoint;
-        this.pause = (Math.random() * 200) + 50;
-        this.dur = (Math.random() * 300) + 3507~zz so;
+        this.pause = ( (Math.random() * 17) + 100 );
+        this.dur = ( (Math.random() * 300) + 200 );
      }
      
      exec() {
@@ -124,11 +136,21 @@ class Interaction {
      }
 }
 
+class Scan {
+    constructor(obj, markMatches) {
+        this.shot = Android.takeScreenshot();
+        this.tgt = new Image(path(obj.asset));
+        this.mask = new Image(path(obj.mask,,true));
+        this.score = obj.score;
+        this.matches = Vision.findMaskedMatches;
+    }
+}
+
 class Asset {
     
     constructor(obj_in, color_in = "red", pix_in = 2) {
-        this.needle = new Image(obj_in.asset);
-        this.mask = new Image(obj_in.mask);
+        this.needle = new Image(path(obj_in.file));
+        this.mask = new Image(path(obj_in.file,,true));
         this.score = obj_in.score;
         this.limit = obj_in.limit;
         this.haystack;
@@ -213,6 +235,7 @@ if (Android.connected()) {
     Helper.sleep(1);
     if (Android.startApp(adv.pak)) {
         Helper.log("Game package found! Initializing surveillance system...");
+        
         main();
     } else {
         Helper.log("Opening game package...");
@@ -229,9 +252,10 @@ function main() {
     }
 }
 
-/*The path() function takes one or two args, corresponding to a file name and the folder within which it should be located. Using a template literal marked with backticks, variable names can be substituted into the string value to be returned. if only a single argument is passed in, the folder defaults to assets/, the location where all the target images are located.*/
-function path(fileName, inFolder = "assets") {
-    return `${inFolder}/${fileName}.png`;
+/*The path() function takes one, two, or three args, corresponding to a file name, the folder within which it should be located, and a Boolean that determines whether "_mask" is appended to the intended name of the file. Using a template literal marked with backticks, variable names are substituted into a string that is then returned which contains a slash and the file type, namely that of a PNG. If only a single argument is passed in, the folder defaults to assets/, the location where all the target images are located, and no mask signifier is appended.*/
+function path(fileName, inFolder = "assets", isMask) {
+    let fullName = isMask ? fileName + "_mask" : fileName;
+    return `${inFolder}/${fullName}.png`;
 }
 
 function prereqSetup(state_in) {
@@ -262,7 +286,7 @@ function getPoint(matches) {
     
 }
 
-function  aim(matches) { //takes array of Matches
+function aim(matches) { //takes array of Matches
     //let hits = []; //creates empty scope-limited array
     for (var targ of matches_in) { //iterate thru array
         let box = targ.getRect(); //derive Rect
@@ -281,3 +305,5 @@ function setInteract(spotsIn, sleepCoef) {
     Helper.msleep(pre);//sleep for duration
     Android.sendTap(spot); //tap spot. Spot is a random point in the Match's screen Rect with random delay before tapping to 'humanize' the outputs
 }
+
+
